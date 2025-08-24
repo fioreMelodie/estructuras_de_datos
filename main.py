b1f45ffaac0dbca2fce5130452237e253ac709b7
fioreMelodie <adrianapsanchez27@gmail.com>
2025-08-23 23:53:28 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# main.py
-from controller import ControladorProducto
-
-if __name__ == "__main__":
-    ctrl = ControladorProducto()
-    ctrl.agregar_producto("Pan", 1500)
-    ctrl.agregar_producto("Leche", 3200)
